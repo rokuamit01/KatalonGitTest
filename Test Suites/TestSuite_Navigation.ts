aba279dd980b22ddbd5f99c290da15aacaf133e2
rokuamit <rokuamit@gmail.com>
2020-07-19 21:21:36 -0400

    Added Test Suite

---
+++ Test Suites/TestSuite_Navigation.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite_Navigation</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5153fee6-7292-4ea7-9ed3-6d571c6e7ecd</testSuiteGuid>
+   <testCaseLink>
+      <guid>9457340a-3522-4135-91a3-4d82ab1ac6a0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test01</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>060f94af-be2f-49b4-b91b-11272568733d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test02</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
